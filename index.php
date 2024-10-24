aeb950d5e478e33a54baa072c058e3a5c0df00fc
Waldemar Paryz <EMAIL>
2024-10-24 09:51:34 +0200

    d

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
 </head>
 <body>
 
-<form action="POST" action="index.php">
+<form method="POST" action="index.php">
     Wpisz nazwisko: <input type="text" name="nazwisko">
     <input type="submit" value="Filtruj">
 </form>
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -22,21 +23,28 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Błąd połączenia: " . mysqli_connect_error());
 }
-?>
 
-<?php
-$sql = "SELECT * FROM uczniowie";
-$result = mysqli_query($conn, $sql);
+if(isset($_POST['nazwisko']) && $_POST['nazwisko'] != '') {
+    $nazwisko = $_POST['nazwisko'];
+    $nazwisko = mysqli_real_escape_string($conn, $nazwisko);
 
-if (mysqli_num_rows($result) > 0) {
-    echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>".$row["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>";
+    $sql = "SELECT * FROM uczniowie WHERE nazwisko='$nazwisko'";
+    
+    $result = mysqli_query($conn, $sql);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>".$row["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Brak wyników";
     }
-    echo "</table>";
 } else {
-    echo "Brak wyników";
+    echo "Nie wpisałeś nazwiska";
 }
+
 mysqli_close($conn);
 ?>
 </body>
